c7b18ab1f0d5fedad9f0a6ba300730b94ca84ee6
abdopcnet <abdopcnet@gmail.com>
2025-10-09 15:48:26 +0300

    Initial commit: user_branch_control

---
+++ user_branch_control/user_branch_control/doctype/user_branch_control/user_branch_control.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, abdopcnet@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("user_branch_control", {
+// 	refresh(frm) {
+
+// 	},
+// });
