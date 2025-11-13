76838e837f4130c67d9e73ef01c26cac86482b8b
abdopcnet <abdopcnet@gmail.com>
2025-11-13 05:10:39 +0300

    Update hooks.py - Add doc_events for Sales Invoice branch validation

--- user_branch_control/hooks.py
+++ user_branch_control/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+    "Sales Invoice": {
+        "before_save": "user_branch_control.strict_user_to_allowed_branch.validate_branch_write_access"
+    }
+}
 
 # Scheduled Tasks
 # ---------------
